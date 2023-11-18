45d2e67eb5cc8def27032a8afff979941b203633
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2023-11-18 19:46:51 +0300

    Weapon animations & handler

---
+++ Assets/Scripts/WeaponScripts/WeaponHandler.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim
+{
+    NONE,
+    AIM
+}
+public enum WeaponBulletType
+{
+    NONE,
+    BULLET
+}
+
+public class WeaponHandler : MonoBehaviour
+{   
+    private Animator _animator;
+    public WeaponAim weaponAim;
+
+    [SerializeField] private GameObject muzzleFLash;
+    [SerializeField] private AudioSource shootSound;
+    [SerializeField] private AudioSource reloadSound;
+    public WeaponBulletType bulletType;
+    public GameObject attackPoint;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Shoot()
+    {
+        _animator.SetTrigger("SwordAttack");
+        shootSound.Play();
+    }
+
+    void Aim(bool canAim)
+    {
+        _animator.SetTrigger("Aim");
+    }
+
+    void MuzzleFlashOn()
+    {
+        muzzleFLash.SetActive(true);
+    }
+    void MuzzleFlashOff()
+    {
+        muzzleFLash.SetActive(false);
+    }
+
+    void Reload()
+    {
+        reloadSound.Play();
+    }
+
+    void TurnOnAttackPoint()
+    {
+        attackPoint.SetActive(true);
+    }
+    
+    void TurnOffAttackPoint()
+    {
+        if(attackPoint.activeInHierarchy)
+        {
+            attackPoint.SetActive(false);
+        }
+    }
+}
